bca7b2e96a5ff0e35ea00f7a55228abef8db36f0
nassef99 <nassefameen@gmail.com>
2022-02-03 19:46:01 -0600

    added attributes and started views

--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
     subject do
-        described_class.new(title: 'harry potter')
+        described_class.new(title: 'harry potter', author: "j.k rowling", price: "15.50", date_published: "1-1-2001")
     end
 
     it 'is valid with valid attributes' do
