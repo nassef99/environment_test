db8f8be2b0dd51b13d56052b1ee07b3288d7fb0d
nassef99 <nassefameen@gmail.com>
2022-02-03 23:45:52 -0600

    final commit

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -5,6 +5,9 @@ RSpec.describe 'Creating a book', type: :feature do
     scenario 'valid inputs' do
         visit new_book_path
         fill_in 'Title', with: 'harry potter'
+        fill_in 'Author', with: 'jk rowling'
+        fill_in 'Price', with: '1'
+        select '2022', :from => 'book_date_published_1i'
         click_on 'Create Book'
         visit books_path
         expect(page).to have_content('harry potter')
@@ -13,6 +16,8 @@ RSpec.describe 'Creating a book', type: :feature do
         visit new_book_path
         fill_in 'Title', with: 'harry potter'
         fill_in 'Author', with: 'jk rowling'
+        fill_in 'Price', with: '1'
+        select '2022', :from => 'book_date_published_1i'
         click_on 'Create Book'
         visit books_path
         expect(page).to have_content('jk rowling')
@@ -20,17 +25,23 @@ RSpec.describe 'Creating a book', type: :feature do
     scenario 'valid price' do
         visit new_book_path
         fill_in 'Title', with: 'harry potter'
-        fill_in 'Price', with: '9.99'
+        fill_in 'Author', with: 'jk rowling'
+        fill_in 'Price', with: '1'
+        select '2022', :from => 'book_date_published_1i'
         click_on 'Create Book'
         visit books_path
-        expect(page).to have_content('9.99')
+        expect(page).to have_content('1')
     end
     scenario 'valid date' do
         visit new_book_path
         fill_in 'Title', with: 'harry potter'
-        fill_in 'Date Published', with: '2001-1-1'
+        fill_in 'Author', with: 'jk rowling'
+        fill_in 'Price', with: '1'
+        select '2022', :from => 'book_date_published_1i'
+        select 'January', :from => 'book_date_published_2i'
+        select '1', :from => 'book_date_published_3i'
         click_on 'Create Book'
         visit books_path
-        expect(page).to have_content('2001-1-1')
+        expect(page).to have_content('2022-01-01')
     end
 end
