680a0c0938f7aaa420596f95736d52c04422a07c
nassef99 <nassefameen@gmail.com>
2022-02-03 21:25:36 -0600

    finished writing rspec tests

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -9,4 +9,28 @@ RSpec.describe 'Creating a book', type: :feature do
         visit books_path
         expect(page).to have_content('harry potter')
     end
+    scenario 'valid author' do
+        visit new_book_path
+        fill_in 'Title', with: 'harry potter'
+        fill_in 'Author', with: 'jk rowling'
+        click_on 'Create Book'
+        visit books_path
+        expect(page).to have_content('jk rowling')
+    end
+    scenario 'valid price' do
+        visit new_book_path
+        fill_in 'Title', with: 'harry potter'
+        fill_in 'Price', with: '9.99'
+        click_on 'Create Book'
+        visit books_path
+        expect(page).to have_content('9.99')
+    end
+    scenario 'valid date' do
+        visit new_book_path
+        fill_in 'Title', with: 'harry potter'
+        fill_in 'Date Published', with: '2001-1-1'
+        click_on 'Create Book'
+        visit books_path
+        expect(page).to have_content('2001-1-1')
+    end
 end
